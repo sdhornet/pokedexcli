4bb14fad2c938ced59e70aa096069da150c4ba49
sdhornet1 <sdhornet1@gmail.com>
2026-05-21 13:28:28 -0500

    Made some progress, need to clean up my error handling

--- main.go
+++ main.go
@@ -6,7 +6,46 @@ import (
 	"os"
 )
 
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
 func main() {
+	commands := getCommands()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("pokedex > ")
@@ -19,7 +58,13 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("Your command was: %s\n", command[0])
+		// revist the command[0] and error management and the if/else
+		elem, ok := commands[command[0]]
+		if ok {
+			elem.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
